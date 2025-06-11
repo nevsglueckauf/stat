740cf6e9e44e40f1c7990e1f1b6919ca3652df6d
Sven Schrodt <sven@schrodt.club>
2025-06-11 15:59:55 +0200

    Adding Unit tests

--- tests/statz_test.py
+++ tests/statz_test.py
@@ -1,10 +1,22 @@
 from mypkg.statz import StatPy
+import statistics
+import unittest
+
+class TestStatPy(unittest.TestCase):
+    """ Unit testing StatPy class versus statistics """
+    
+    def test_median(self):
+        income = [30000, 5000, 6000, 3000, 4000, 5000, 5000, 8000, 6000]
 
 
-d = [30000, 5000, 6000, 3000, 4000, 5000, 5000, 8000, 6000]
-
-m = StatPy.clc_mode(d)
-
-print(m)
+        median = StatPy.clc_med(income)
+        median_stat = statistics.median(income)
+        avg = StatPy.clc_avg(income)
+        avg_stat = statistics.mean(income)
+        mode = StatPy.clc_mode(income)
+        mode_stat = statistics.mode(income)
+        self.assertEqual(median_stat, median_stat, 'Fehler!')
+        self.assertEqual(avg_stat, avg_stat, 'Fehler!')
+        self.assertEqual(mode, mode_stat, 'Fehler!')
 
 
