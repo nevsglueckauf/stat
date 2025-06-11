538e078a2864a1446bb365c745fd50656565f8dd
Sven Schrodt <sven@schrodt.club>
2025-06-11 13:16:24 +0200

    Adding stuff

---
+++ normvert.py
@@ -0,0 +1,26 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+from scipy.stats import norm
+
+# Parameter der normalverteilung  
+# mittelwert μ
+mu = 166 # erwartungswert = mittelwert für körpergröße Frauen Deutschland 
+sigma = 10 # Standardabweichung
+
+# Dichtefunktion, also hier Normalverteilung 
+x = np.linspace(mu - 4 * sigma, mu + 4 * sigma, 1000) 
+pdf = norm.pdf(x, mu, sigma) # Achtung pdf nichts mit pdf zu, steht für PROPABILITY DENSITY FUNCTION (WahrscheinlichkeitsDichteFunktion)
+
+# Wahrscheinlichkeit P(X >= 180)
+x_threshold = 180
+prob_ge_threshold = norm.sf(x_threshold, mu, sigma) # Survival Funktion später noch wichtig
+                            
+# Plot 
+fig, ax = plt.subplots()
+ax.plot(x, pdf, label=r"$N(166, 10)")
+ax.fill_between(x[x >= x_threshold], pdf[x >= x_threshold], alpha = 0.3)
+ax.set_xlabel("Körpergröße")
+ax.set_ylabel("Dichte")
+ax.set_title(f"P(X>={x_threshold}cm) = {prob_ge_threshold:.3%}")
+ax.legend()
+plt.show()
