e79467dccd672584d45ef2dad4ca7136ffce6242
Sven Schrodt <sven@schrodt.club>
2025-06-13 10:35:42 +0200

    Adding stuff

--- mypkg/mathzwo.py
+++ mypkg/mathzwo.py
@@ -7,10 +7,10 @@ class MathZwo:
     def factorial(n:int) ->int:
         """ Berechnung der Fakultät (factorial)
         
-            -$ n!=n⋅(n−1)⋅(n−2)⋅…⋅1$
+            - n!=n⋅(n−1)⋅(n−2)⋅…⋅1
 
         Args:
-            n (int): _descrZAhl, deren Fakultät berrechnet werden soll
+            n (int): Zahl, deren Fakultät berrechnet werden soll
 
         Returns:
             int: Fakultät von n
